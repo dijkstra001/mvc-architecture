08143dbcb3a88774dd70ac5157a6db0fd431dac5
dijkstra001 <ncorreiatrabalhando@gmail.com>
2021-06-22 11:32:39 -0300

    Created project

---
+++ src/main/java/com/mvc/MVCarquiteture/MvcArquitetureApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.MVCarquiteture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcArquitetureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcArquitetureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvc/MVCarquiteture/MvcArquitetureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc.MVCarquiteture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcArquitetureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
